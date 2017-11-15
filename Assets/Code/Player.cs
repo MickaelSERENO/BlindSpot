aef43d4f45cdf6767f3809ecb0e082ba4b1fe956
Tlospock <meunierpaul@free.fr>
2017-11-15 21:49:32 +0100

    changement colume

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿jeusing System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -91,7 +91,7 @@ public class Player : MonoBehaviour
 			//Compute and push azimuth + altitude for every sound source
 			PDData data = ComputeAngle(obj);
 			if(!ss.Lighted)
-				data.Volume = 47;
+				data.Volume = 60;
 			else
 				data.Volume = 0;
 			m_datas.Add(data);
